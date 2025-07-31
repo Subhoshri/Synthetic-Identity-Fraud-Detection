74a1f0799dd25030f170f55b55a7b02bec2440b7
Subhoshri Pal <138369802+Subhoshri@users.noreply.github.com>
2025-07-31 22:57:41 +0530

    improved graph

--- graph.py
+++ graph.py
@@ -1,26 +1,26 @@
 import pandas as pd
 import networkx as nx
 import pickle
-import networkx as nx
-import pandas as pd
 
 df = pd.read_csv("Dataset/Base_with_identifiers.csv")
-
+if "user_id" not in df.columns:
+    df["user_id"]=df.index
+df.to_csv("Dataset/Base_with_identifiers.csv", index=False)
 G = nx.Graph()
 
-# Add nodes
 for idx, row in df.iterrows():
-    G.add_node(row["user_id"], fraud=row["fraud_bool"])
+    G.add_node(f"user_{row["user_id"]}", fraud=row["fraud_bool"])
 
-# Add edges based on shared identifiers
 identifier_cols = ["email", "phone_number", "device_id", "ip_address"]
 
 for col in identifier_cols:
     shared = df.groupby(col)["user_id"].apply(list)
     for user_list in shared:
-        for i in range(len(user_list)):
-            for j in range(i + 1, len(user_list)):
-                G.add_edge(user_list[i], user_list[j], link=col)
+        if len(user_list) > 1:
+            identifier_node = f"id_{col}_{user_list[0]}"
+            G.add_node(identifier_node)
+            for uid in user_list:
+                G.add_edge(f"user_{uid}", identifier_node, link=col)
 
 with open("models/user_graph.gpickle", "wb") as f:
     pickle.dump(G, f)
